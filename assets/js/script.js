d6789d744569a2a5ba286c67c03617daddca6486
Maiga Boubacar <maiga@MacBook-Pro-de-Maiga.local>
2020-02-24 18:02:49 +0100

    add creating todo functionnality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
 // Check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed") 
 });
 
 // Click on X to delete Todo
-$("span").click(function(e){
+$("ul").on("click", "span", function(e){
     // remove span parent and not just span
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     e.stopPropagation();
+});
+
+// When you write something into input and press enter : Add ToDo
+var text = $("ul").html();
+$("input[type='text']").keypress(function(e){
+    if (e.which === 13){
+        $("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+        $(this).val("");
+    }
 });
