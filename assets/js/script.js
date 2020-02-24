41853283de00d921b0344866d2395c505b6c9852
Maiga Boubacar <maiga@MacBook-Pro-de-Maiga.local>
2020-02-24 20:38:33 +0100

    add css

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ $("ul").on("click", "span", function(e){
 var text = $("ul").html();
 $("input[type='text']").keypress(function(e){
     if (e.which === 13){
-        $("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + $(this).val() + "</li>");
         $(this).val("");
     }
 });
