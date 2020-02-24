6074c649f23c6a3466ab0eef3639434f0fd951a9
Maiga Boubacar <maiga@MacBook-Pro-de-Maiga.local>
2020-02-24 15:16:24 +0100

    add delete functionnality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
-// check off sêcific todos by clicking
+// Check off specific todos by clicking
 $("li").click(function(){
     $(this).toggleClass("completed") 
+});
+
+// Click on X to delete Todo
+$("span").click(function(e){
+    // remove span parent and not just span
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
 });
